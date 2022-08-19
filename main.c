ae2ac6c81f8240674dd1beb28748d1080f36b0c4
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-08-19 09:31:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ printf ("Um anel para todos governar");
   printf("é bolacha ou biscoito: corrigindo");
   printf("É o Magas");
   printf("giovani: agora esta certo");
+  printf("só testando mesmo");
 return 0;
 }
