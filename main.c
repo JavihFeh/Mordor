9f8e67d2a7e6a4fc64347e66f19a10760a5329ad
MAgalDI02 <102761482+MAgalDI02@users.noreply.github.com>
2022-08-19 09:29:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main ()
 {
 printf ("Um anel para todos governar");
   printf("é bolacha ou biscoito: corrigindo");
-  
+  printf("É o Magas");
   printf("giovani: agora esta certo");
 return 0;
 }
